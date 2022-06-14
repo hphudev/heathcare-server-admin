98d7b35bac1d6c4b7221413e09be15c7ec04d18c
ds <19520216@gm.uit.edu.vn>
2022-06-14 09:24:04 +0700

    Update user controller

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends AdminController
      *
      * @var string
      */
-    protected $title = 'Người dùng';
+    protected $title = 'User';
 
     /**
      * Make a grid builder.
@@ -25,14 +25,20 @@ class UserController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new User());
+
         $grid->column('id', __('ID'))->sortable();
-        $grid->column('name', __('Họ tên'))->sortable();
+        $grid->column('name', __('Name'))->sortable();
+        $grid->column('province', __('Province'))->sortable();
+        $grid->column('district', __('District'))->sortable();
+        $grid->column('ward', __('Ward'))->sortable();
+        $grid->column('detail_address', __('Detail address'))->sortable();
         $grid->column('email', __('Email'))->sortable();
-        $grid->column('email_verified_at', __('Email verified at'))->sortable();
+        $grid->column('email_verified_at', __('Email verified at'));
         // $grid->column('password', __('Password'));
         // $grid->column('remember_token', __('Remember token'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        // $grid->column('created_at', __('Created at'))->sortable();
+        $grid->column('updated_at', __('Updated at'))->sortable();
+
         return $grid;
     }
 
@@ -48,12 +54,17 @@ class UserController extends AdminController
 
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
+        $show->field('province', __('Province'));
+        $show->field('district', __('District'));
+        $show->field('ward', __('Ward'));
+        $show->field('detail_address', __('Detail address'));
         $show->field('email', __('Email'));
         $show->field('email_verified_at', __('Email verified at'));
-        // $show->field('password', __('Password'));
-        // $show->field('remember_token', __('Remember token'));
+        $show->field('password', __('Password'));
+        $show->field('remember_token', __('Remember token'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
+
         return $show;
     }
 
@@ -65,11 +76,17 @@ class UserController extends AdminController
     protected function form()
     {
         $form = new Form(new User());
+
         $form->text('name', __('Name'));
+        $form->text('province', __('Province'));
+        $form->text('district', __('District'));
+        $form->text('ward', __('Ward'));
+        $form->text('detail_address', __('Detail address'));
         $form->email('email', __('Email'));
-        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        // $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
         $form->password('password', __('Password'));
-        $form->text('remember_token', __('Remember token'));
+        // $form->text('remember_token', __('Remember token'));
+
         return $form;
     }
 }
